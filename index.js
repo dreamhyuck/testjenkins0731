5bbb88140ff28d103a52cc3bf16732d9393aa236
dreamhyuck <68985417+dreamhyuck@users.noreply.github.com>
2020-07-31 14:45:13 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like Jenkinstest!');
+    res.send('I like Jenkinstest02!');
 });
 
 app.get('/start', function(req, res){
